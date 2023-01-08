46624ab1a21ad4e46a63618b9b4c63b81c5ad652
takaya suzuki <suzuki8522@yahoo.co.jp>
2023-01-08 17:34:20 +0900

    🔧state複数使用

--- src/040_multiple_state/start/Example.js
+++ src/040_multiple_state/start/Example.js
@@ -1,5 +1,39 @@
+import { useState } from "react";
+
 const Example = () => {
-  
+  const [countA, setCountA] = useState(0);
+  const [countB, setCountB] = useState(10);
+  const [countC, setCountC] = useState(100);
+  return (
+    <>
+      <p>ボタンを{countA}回押しました！</p>
+      <button
+        onClick={() => {
+          setCountA(countA + 1);
+        }}
+      >
+        countAボタン
+      </button>
+
+      <p>ボタンを{countB}回押しました！</p>
+      <button
+        onClick={() => {
+          setCountB(countB + 1);
+        }}
+      >
+        countBボタン
+      </button>
+
+      <p>ボタンを{countC}回押しました！</p>
+      <button
+        onClick={() => {
+          setCountC(countC + 1);
+        }}
+      >
+        countCボタン
+      </button>
+    </>
+  );
 };
 
 export default Example;
